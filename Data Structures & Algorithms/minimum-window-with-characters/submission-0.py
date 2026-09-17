8a86e785842d349cb3989559230c252c7459818d
Leo <leomorganpro@gmail.com>
2026-09-17 17:26:01 +0100

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        substring = s
+        right = 0
+        left = 0
+        tally = 0
+        valid = False
+        tFreq = {}
+        for char in t:
+            if char in tFreq:
+                tFreq[char] += 1
+            else:
+                tFreq[char] = 1
+        
+        sFreq = {}
+
+        while right < len(s):
+            if s[right] in tFreq:
+                if s[right] in sFreq:
+                    sFreq[s[right]] += 1
+                else:
+                    sFreq[s[right]] = 1
+                
+                if sFreq[s[right]] == tFreq[s[right]]:
+                    tally += 1
+                
+            while tally == len(tFreq.keys()):
+                valid = True
+                if s[left] in tFreq:
+                    sFreq[s[left]] -= 1
+                    if sFreq[s[left]] < tFreq[s[left]]:
+                        tally -= 1
+                        
+                        if(right - left + 1) < len(substring):
+                            substring = s[left:right+1]
+                left += 1
+
+            right += 1
+        
+        if valid:
+            return substring
+        return ""
